97596c6e4f4f2427b79245980e7ec30e6643a569
Bharath <bharath@doshi.app>
2024-12-24 17:16:35 +0530

    first commit

---
+++ src/main/kotlin/com/thockytech/blocease/BlocEaseContext.kt
@@ -0,0 +1,7 @@
+package com.thockytech.blocease.liveTemplates
+
+import com.intellij.codeInsight.template.TemplateContextType
+
+class BlocEaseContext : TemplateContextType("BlocEase", "Bloc Ease") {
+
+}
